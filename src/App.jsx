b3fb3005dc4aff11de2c7c2c4d7316dfb62efd62
Asser-wael <aserweal@gmail.com>
2026-07-31 02:34:39 +0300

    Add React project

--- src/App.jsx
+++ src/App.jsx
@@ -85,6 +85,8 @@ export default function App() {
   useEffect(() => {
     if (user) {
       subscribeToPush();
+    dispatch(setNotification({ message: "done", type: "sccess" }));
+      
     }
   }, [user]);
   useEffect(() => {
